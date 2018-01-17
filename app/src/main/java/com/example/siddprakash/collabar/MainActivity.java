f6898746302ebd43c7017ffe0acc94d1ff4c427c
Siddhant Prakash <sid.rol92@gmail.com>
2018-01-16 19:53:39 -0700

    opencv imported

--- app/src/main/java/com/example/siddprakash/collabar/MainActivity.java
+++ app/src/main/java/com/example/siddprakash/collabar/MainActivity.java
@@ -2,18 +2,34 @@ package com.example.siddprakash.collabar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import org.opencv.android.OpenCVLoader;
+
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "OpenCVDebug";
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+        System.loadLibrary("opencv_java3");
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV Initialized!");
+        }
+        else{
+            Log.d(TAG, "OpenCV Initialization Failed!");
+        }
+
+
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
