b78b48536d5a2431d83b2fa01daaf06ae39aa527
Siddhant Prakash <sid.rol92@gmail.com>
2018-01-18 16:32:50 -0700

    basic camera app

--- app/src/main/java/com/example/siddprakash/collabar/MainActivity.java
+++ app/src/main/java/com/example/siddprakash/collabar/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.siddprakash.collabar;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
 
-    String TAG = "OpenCVDebug";
+    private static final int REQUEST_WRITE_CAMERA_PERMISSION = 200;
+    private int permission;
+
+
+    private String TAG = "OpenCVDebug";
+    private JavaCameraView javaCameraView;
+    private Mat myFrame;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -17,10 +37,35 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("opencv_java3");
     }
 
+    BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status){
+                case BaseLoaderCallback.SUCCESS:{
+                    javaCameraView.enableView();
+                    break;
+                }
+                default:{
+                    super.onManagerConnected(status);
+                }
+            }
+            super.onManagerConnected(status);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA},
+                    REQUEST_WRITE_CAMERA_PERMISSION);
+        }
 
         if(OpenCVLoader.initDebug()){
             Log.d(TAG, "OpenCV Initialized!");
@@ -28,18 +73,85 @@ public class MainActivity extends AppCompatActivity {
         else{
             Log.d(TAG, "OpenCV Initialization Failed!");
         }
-
-
         setContentView(R.layout.activity_main);
 
+
+
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
+
+        javaCameraView = (JavaCameraView)findViewById(R.id.java_camera_view);
+        javaCameraView.setVisibility(View.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_WRITE_CAMERA_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Write to the storage (ex: call appendByteBuffer(byte[] data) here)
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please grant permission.", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(javaCameraView != null){
+            javaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(javaCameraView != null){
+            javaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV Initialized!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+        else{
+            Log.d(TAG, "OpenCV Initialization Failed!");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
+        }
+    }
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        myFrame = new Mat(width, height, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        myFrame.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        myFrame = inputFrame.rgba();
+        return myFrame;
+    }
 }
