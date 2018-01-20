c2cd668157d5be1305e996f83e39b4d351c5fb83
Siddhant Prakash <sid.rol92@gmail.com>
2018-01-20 11:43:51 -0700

    update camera

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,33 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
 
+using namespace cv;
 extern "C"
-JNIEXPORT jstring JNICALL
+JNIEXPORT void JNICALL
 Java_com_example_siddprakash_collabar_MainActivity_stringFromJNI(
         JNIEnv *env,
-        jobject /* this */) {
+        jobject /* this */,
+        jlong addrRgba, jlong addrGray) {
+
+
+    Mat &mRgb = *(Mat*)addrRgba;
+    Mat &mGray = *(Mat*)addrGray;
+
+    cvtColor(mRgb, mGray, CV_RGB2GRAY);
+
+//    Mat img = imread("/mnt/sdcard/Android/Data/CollabAR/image.jpg");
+
+    /*
+    mGray.data = img.data;
+    mGray = img;
+    cvCopy(&img, &mGray);
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+     */
+    return;
+
 }
--- app/src/main/java/com/example/siddprakash/collabar/MainActivity.java
+++ app/src/main/java/com/example/siddprakash/collabar/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.siddprakash.collabar;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.os.Build;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -20,16 +22,26 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
 
     private static final int REQUEST_WRITE_CAMERA_PERMISSION = 200;
     private int permission;
 
+    private String appFolderPath = Environment.getExternalStorageDirectory() + "/Android/Data/CollabAR/";
+
 
     private String TAG = "OpenCVDebug";
     private JavaCameraView javaCameraView;
-    private Mat myFrame;
+    private Mat myFrame, newFrame;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -75,7 +87,8 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         }
         setContentView(R.layout.activity_main);
 
-
+        CreateAppFolderIfNeed();
+        copyAssetsDataIfNeed();
 
         // Example of a call to a native method
 //        TextView tv = (TextView) findViewById(R.id.sample_text);
@@ -137,7 +150,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native void stringFromJNI(long matAddrRgba, long matAddrGray);
 
     @Override
     public void onCameraViewStarted(int width, int height) {
@@ -152,6 +165,72 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         myFrame = inputFrame.rgba();
-        return myFrame;
+        newFrame = new Mat();
+        //Imgproc.cvtColor(newFrame,newFrame,Imgproc.COLOR_BGR2BGRA);
+        stringFromJNI(myFrame.getNativeObjAddr(), newFrame.getNativeObjAddr());
+        return newFrame;
+    }
+
+    private void CreateAppFolderIfNeed(){
+        Log.d(TAG, appFolderPath);
+        File folder = new File(appFolderPath);
+        boolean success = true;
+        if (!folder.exists()) {
+            success = folder.mkdirs();
+            if(!success)
+                Log.d(TAG,"App folder does not exist");
+            else
+                Log.d(TAG,"App folder created!");
+        } else {
+            Log.d(TAG,"App folder exists");
+        }
+    }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
+
+    private boolean copyAsset(AssetManager assetManager, String fromAssetPath, String toPath) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            assetManager = getAssets();
+            in = assetManager.open(fromAssetPath);
+            new File(toPath).createNewFile();
+            out = new FileOutputStream(toPath);
+            copyFile(in, out);
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+            return true;
+        } catch(Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "[ERROR]: copyAsset: unable to copy file = "+fromAssetPath);
+            return false;
+        }
+    }
+
+    private void copyAssetsDataIfNeed(){
+        String assetsToCopy[] = {"image.jpg", "marker.jpg"};
+        for(int i=0; i<assetsToCopy.length; i++){
+            String from = assetsToCopy[i];
+            String to = appFolderPath+from;
+
+            // 1. check if file exist
+            File file = new File(to);
+            if(file.exists()){
+                Log.d(TAG, "copyAssetsDataIfNeed: file exist, no need to copy:"+from);
+            } else {
+                // do copy
+                boolean copyResult = copyAsset(getAssets(), from, to);
+                Log.d(TAG, "copyAssetsDataIfNeed: copy result = " + copyResult + " of file = " + from);
+            }
+        }
     }
 }
